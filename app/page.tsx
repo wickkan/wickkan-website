c304dced68e4d42fafd517532b4e394d9796b443
Kanishka <kanishkawickramasinghe50@gmail.com>
2024-05-07 15:54:19 +1000

    changed homepage

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,16 @@ export default function Page() {
         Kanishka Wickramasinghe
       </h1>
       <p className="mb-4">
-        {`Hi there, I am studying Commerce and Computer Science and I have a passion for finance and data science.`}
+        {`Hi there, my name is Kanishka and I'm currently studying Commerce and Computer Science at Monash University.`}
       </p>
       <p className="mb-4">
-        {`Click on the links below to learn more about me.`}
+        {`Throughout my time at Monash I've been fortunate enough to gain experience in the domains of data science and finance both in and outside of university.`}
+      </p>
+      <p className="mb-4">
+        {`I created this website as a tribute to my experiences at Monash.`}
+      </p>
+      <p className="mb-4">
+        {`Click on the links below to learn more about me!`}
       </p>
       <div className="my-8">
       </div>
